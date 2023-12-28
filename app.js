97e0554cd38f6feea7727be0b276641e41a58449
Maksl18 <80098617+Maksl18@users.noreply.github.com>
2023-12-28 17:06:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,121 @@
-const item = document.querySelector('.item')
-const placeholders = document.querySelectorAll('.placeholder')
+document.addEventListener('DOMContentLoaded', () => {
+    const gameBoard = document.querySelector('.game-board');
+    const gridSize = 30;
+    let snake = [{ x: 10, y: 10 }];
+    let food = { x: 15, y: 15 };
+    let direction = 'right';
+    let gameInterval;
 
-item.addEventListener('dragstart', dragstart)
-item.addEventListener('dragend', dragend)
+    function drawBoard() {
+      for (let y = 1; y <= gridSize; y++) {
+        for (let x = 1; x <= gridSize; x++) {
+          const cell = document.createElement('div');
+          cell.classList.add('cell');
+          cell.dataset.x = x;
+          cell.dataset.y = y;
+          gameBoard.appendChild(cell);
+        }
+      }
+    }
 
-for (const placeholder of placeholders) {
- placeholder.addEventListener('dragover', dragover)
- placeholder.addEventListener('dragenter', dragenter)
- placeholder.addEventListener('dragleave', dragleave)
- placeholder.addEventListener('drop', dragdrop)
-}
+    function drawSnake() {
+      snake.forEach(segment => {
+        const snakeCell = document.querySelector(`[data-x="${segment.x}"][data-y="${segment.y}"]`);
+        snakeCell.classList.add('snake');
+      });
+    }
 
-function dragstart(event) {
- event.target.classList.add('hold')
- setTimeout(() => event.target.classList.add('hide'), 0)
-}
+    function drawFood() {
+      const foodCell = document.querySelector(`[data-x="${food.x}"][data-y="${food.y}"]`);
+      foodCell.classList.add('food');
+    }
 
-function dragend(event) {
- event.target.className = 'item'
-}
+    function moveSnake() {
+      const head = { ...snake[0] };
+      switch (direction) {
+        case 'up':
+          head.y--;
+          break;
+        case 'down':
+          head.y++;
+          break;
+        case 'left':
+          head.x--;
+          break;
+        case 'right':
+          head.x++;
+          break;
+      }
+      snake.unshift(head);
+      if (head.x === food.x && head.y === food.y) {
+        generateFood();
+      } else {
+        snake.pop();
+      }
+      checkCollision();
+    }
 
-function dragover(event) {
-    event.preventDefault()
-}
+    function generateFood() {
+      const newFoodX = Math.floor(Math.random() * gridSize) + 1;
+      const newFoodY = Math.floor(Math.random() * gridSize) + 1;
+      food = { x: newFoodX, y: newFoodY };
+    }
 
-function dragenter(event) {
-    event.target.classList.add('hovered')
-}
+    function updateGame() {
+      gameBoard.querySelectorAll('.cell').forEach(cell => {
+        cell.className = 'cell';
+      });
+      drawSnake();
+      drawFood();
+      moveSnake();
+    }
 
-function dragleave(event) {
-    event.target.classList.remove('hovered')
-}
+    function handleKeyPress(event) {
+      const key = event.key.toLowerCase();
+      if (key === 'w' || key === 'arrowup') {
+        direction = 'up';
+      } else if (key === 's' || key === 'arrowdown') {
+        direction = 'down';
+      } else if (key === 'a' || key === 'arrowleft') {
+        direction = 'left';
+      } else if (key === 'd' || key === 'arrowright') {
+        direction = 'right';
+      }
+    }
 
-function dragdrop(event) {
-    event.target.classList.remove('hovered')
-    event.target.append(item)
-}
+    function checkCollision() {
+      const head = snake[0];
+      for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+          endGame();
+          break;
+        }
+      }
+    }
+
+    function endGame() {
+      clearInterval(gameInterval);
+      const restart = confirm('Гра закінчена! Бажаєте розпочати нову гру?');
+      if (restart) {
+        initializeGame();
+      }
+    }
+
+    function initializeGame() {
+      clearInterval(gameInterval); 
+      snake = [{ x: 10, y: 10 }];
+      food = { x: 15, y: 15 };
+      direction = 'right';
+      gameInterval = setInterval(updateGame, 200);
+    }
+
+    drawBoard();
+    drawSnake();
+    drawFood();
+
+    document.getElementById('restartButton').addEventListener('click', () => {
+      initializeGame();
+    });
+
+    document.addEventListener('keydown', handleKeyPress);
+  });
